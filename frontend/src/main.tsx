b6730f0d70cb48a2eddf2f71b8a2ecdcbc1a3977
Valerii Vynokurov <@DESKTOP-0QK6HFI>
2026-05-31 03:28:53 +0300

    Set up Bootstrap and SCSS frontend foundation

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,7 +4,6 @@ import "./styles/app.scss";
 
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
